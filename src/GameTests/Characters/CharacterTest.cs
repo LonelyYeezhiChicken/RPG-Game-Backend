c60041a8ac36226ae4b3794ace2a0e505b1bd79a
Duke <dukelin@va7.com.tw>
2022-01-31 02:01:36 +0800

    feat: 攻擊

--- src/GameTests/Characters/CharacterTest.cs
+++ src/GameTests/Characters/CharacterTest.cs
@@ -1,5 +1,7 @@
 ﻿using GameModels.Characters;
+using GameModels.Characters.Models;
 using GameModels.Characters.Models.enums;
+using GameModels.Characters.Skills;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
@@ -9,20 +11,58 @@ namespace GameTests.Characters
     [TestFixture]
     class CharacterTest
     {
+        public AbilityDto GetAbilityDto()
+        {
+            AbilityDto abilityDto = new AbilityDto();
+            abilityDto.Level = 1;
+            abilityDto.Health = 2000;
+            abilityDto.Magic = 1000;
+            abilityDto.ActionPoint = 50;
+            abilityDto.Strength = 100;
+            abilityDto.Intelligence = 50;
+            abilityDto.Accuracy = 50;
+            abilityDto.Luck = 50;
+            abilityDto.Agility = 50;
+            abilityDto.Attack = 50;
+            abilityDto.Mattack = 50;
+            return abilityDto;
+        }
+
         [Test]
         public void 角色數據()
         {
-            Character character = new Character(OccupationEnum.Mage);
+            Character character = new Character(OccupationEnum.Mage, new StrAttack());
             Console.WriteLine("法師 : ");
             Console.WriteLine(JsonConvert.SerializeObject(character.Create()));
 
-            character = new Character(OccupationEnum.Warrior);
+            character = new Character(OccupationEnum.Warrior, new StrAttack());
             Console.WriteLine("戰士 : ");
             Console.WriteLine(JsonConvert.SerializeObject(character.Create()));
 
-             character = new Character(OccupationEnum.Tank);
+            character = new Character(OccupationEnum.Tank, new StrAttack());
             Console.WriteLine("坦克 : ");
             Console.WriteLine(JsonConvert.SerializeObject(character.Create()));
+        }
+
+        [Test]
+        public void 攻擊()
+        {
+            Character character = new Character(OccupationEnum.Mage, new StrAttack());
+            //我方
+            AbilityDto userAbility = GetAbilityDto();
+            //敵方
+            AbilityDto othertAbility = GetAbilityDto();
+            try
+            {
+                AbilityDto ability = character.SkillAttack(othertAbility, ref userAbility);
+                Console.WriteLine("對手能力");
+                Console.WriteLine(JsonConvert.SerializeObject(ability));
+                Console.WriteLine("自身能力");
+                Console.WriteLine(JsonConvert.SerializeObject(userAbility));
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
